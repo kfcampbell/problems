cf16ff3e9e660d5cb3364d3346ac123282756dc9
kfcampbell <keeg4n.campbell@gmail.com>
2017-06-25 18:01:22 -0700

    Added IntegerSwap.

---
+++ Miscellaneous/IntegerSwap/IntegerSwap.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace IntegerSwap
+{
+    class IntegerSwap
+    {
+        static void Main(string[] args)
+        {
+            int a = 150;
+            int b = 250;
+            Swap(a, b);
+
+            a = 72;
+            b = 26;
+            Swap(a, b);
+
+            a = 8;
+            b = 8;
+            Swap(a, b);
+
+            a = -86;
+            b = 26;
+            Swap(a, b);
+
+            a = -100;
+            b = -86;
+            Swap(a, b);
+        }
+
+        private static void Swap(int a, int b)
+        {
+            Console.WriteLine($"Before swap: a == {a}, b == {b}");
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine($"After swap: a == {a}, b == {b}");
+        }
+    }
+}
