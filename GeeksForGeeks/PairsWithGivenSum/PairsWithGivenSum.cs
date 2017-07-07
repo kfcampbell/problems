bd96a9f97f902bc4018a0625f2417b3e5d27cc38
kfcampbell <keeg4n.campbell@gmail.com>
2017-07-06 21:40:49 -0700

    Added PairsWithGivenSum.

---
+++ GeeksForGeeks/PairsWithGivenSum/PairsWithGivenSum.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PairsWithGivenSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] {5, 2, 3, 8, 9, 15, 29, 7, 10};
+            PrintPairsWithGivenSum(array, 17);
+        }
+
+        static void PrintPairsWithGivenSum(int[] array, int sum)
+        {
+            List<int> nums = array.ToList();
+            nums.Sort();
+
+            int leftPointer = 0;
+            int rightPointer = nums.Count - 1;
+
+            while(leftPointer < rightPointer)
+            {
+                if(nums[leftPointer] + nums[rightPointer] == sum)
+                {
+                    Console.WriteLine($"Pair found: {nums[leftPointer]} + {nums[rightPointer]}");
+                    leftPointer++; // arbitrary?
+                }
+                else if(nums[leftPointer] + nums[rightPointer] < sum)
+                {
+                    leftPointer++;
+                }
+                else
+                {
+                    rightPointer--;
+                }
+            }
+        }
+    }
+}
