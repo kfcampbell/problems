ae544ffbe24422cf2fc1ce4fb8a6bd0b2032b316
kfcampbell <keeg4n.campbell@gmail.com>
2017-07-10 19:56:30 -0700

    Added Anagrams.

---
+++ Miscellaneous/Anagrams/Anagrams.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace Anagrams
+{
+    // given two strings of only lowercase letters, determine if they're anagrams.
+    class Anagrams
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"face and cafe are anagrams: {AreAnagrams("face", "cafe")}");
+            Console.WriteLine($"test and txst are anagrams: {AreAnagrams("test", "txst")}");
+
+            Console.WriteLine($"face and cafe are anagrams: {AreAnagramsConstantSpace("face", "cafe")}");
+            Console.WriteLine($"test and txst are anagrams: {AreAnagramsConstantSpace("test", "txst")}");
+        }
+
+        static bool AreAnagrams(string first, string second)
+        {
+            if (first.Length != second.Length) return false;
+            int[] firstCount = new int[26];
+            int[] secondCount = new int[26];
+
+            foreach (char letter in first)
+            {
+                firstCount[ConvertCharToIntIndex(letter)]++;
+            }
+
+            foreach (char letter in second)
+            {
+                secondCount[ConvertCharToIntIndex(letter)]++;
+            }
+
+            return firstCount.SequenceEqual(secondCount);
+        }
+
+        static bool AreAnagramsConstantSpace(string first, string second)
+        {
+            if (first.Length != second.Length) return false;
+            int[] count = new int[26];
+
+            foreach (char letter in first)
+            {
+                count[ConvertCharToIntIndex(letter)]++;
+            }
+
+            foreach (char letter in second)
+            {
+                count[ConvertCharToIntIndex(letter)]--;
+            }
+
+            foreach(int num in count)
+            {
+                if(num != 0) return false;
+            }
+            return true;
+        }
+
+        static int ConvertCharToIntIndex(char letter)
+        {
+            return (int)letter - 97;
+        }
+    }
+}
