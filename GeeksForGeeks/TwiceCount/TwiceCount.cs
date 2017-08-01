3b0d510a9aa43467f2341902b7054852dfecb8da
kfcampbell <keeg4n.campbell@gmail.com>
2017-07-31 21:39:30 -0700

    Added TwiceCount.

---
+++ GeeksForGeeks/TwiceCount/TwiceCount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwiceCount
+{
+    class TwiceCount
+    {
+        static void Main(string[] args)
+        {
+            string[] words = new string[] { "Robinson", "Canó", "Nelson", "Cruz", "Robinson", "Canó", "Edwin", "Díaz", "Robinson", "Canó", "Edwin", "Díaz" };
+            var count = CountWords(words);
+            PrintWordCounts(count);
+        }
+
+        public static Dictionary<string, int> CountWords(string[] words)
+        {
+            Dictionary<string, int> count = new Dictionary<string, int>();
+
+            foreach(var word in words)
+            {
+                if(count.ContainsKey(word))
+                {
+                    int wordCount = 0;
+                    count.TryGetValue(word, out wordCount);
+                    count[word] = ++wordCount;
+                }
+                else
+                {
+                    count.Add(word, 1);
+                }
+            }
+            return count;
+        }
+
+        public static void PrintWordCounts(Dictionary<string, int> count)
+        {
+            foreach(KeyValuePair<string, int> pair in count)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value}");
+            }
+        }
+    }
+}
